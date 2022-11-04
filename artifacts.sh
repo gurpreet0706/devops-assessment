c16bb9ab7ce29220407e4caad21bc741765994b0
root <root@ip-172-31-95-140.ec2.internal>
2022-11-04 11:36:46 +0000

    abcd

--- artifacts.sh
+++ artifacts.sh
@@ -4,8 +4,8 @@ pwd
 ls
 chmod -R 777 ./Dockerfile
 echo "Building Docker image"
-docker build -t assignment/springapp:$BUILD_NUMBER .
+docker build -t demo/springapp:$BUILD_NUMBER .
 echo " Login Dockerhub"
 echo $DOCKERHUB_CREDENTIALS_PSW | docker login -u $DOCKERHUB_CREDENTIALS_USR --password-stdin
-docker push assignment/springapp:$BUILD_NUMBER
+docker push demo/springapp:$BUILD_NUMBER
 docker logout
