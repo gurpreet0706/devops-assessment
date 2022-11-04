15fa8d2bab20b2837cb18e2c1eccb04a01d615d3
root <root@ip-172-31-95-140.ec2.internal>
2022-11-04 09:56:43 +0000

    added node

--- Jenkinsfile
+++ Jenkinsfile
@@ -3,6 +3,7 @@ pipeline {
     tools {
         maven 'Maven 3.3.9'
         jdk 'java-11'
+        nodejs "node"    
     }
     stages { 
         stage('Intial') { 
--- build.sh
+++ build.sh
@@ -3,4 +3,9 @@
 cd my-app
 mvn clean install
 cd target
-#java -jar my-app-0.0.1-SNAPSHOT.jar
+pushd .
+nohup java -jar my-app-0.0.1-SNAPSHOT.jar > /dev/null 2>&1 &
+serverPID=$!
+newman run ../../DevOps-Assessment.postman_collection.json
+kill serverPID
+
