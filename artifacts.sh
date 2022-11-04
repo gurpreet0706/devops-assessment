48bcce49fe2f392fe86050740998ea6cbd3609bf
root <root@ip-172-31-95-140.ec2.internal>
2022-11-04 11:20:38 +0000

    pushing new changes

--- artifacts.sh
+++ artifacts.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 echo artifacts building
 pwd
+ls
+chmod -R 777 ./Dockerfile
 echo "Building Docker image"
 docker build -t assignment/springapp:$BUILD_NUMBER .
-echo " Lign Dockerhub"
+echo " Login Dockerhub"
 echo $DOCKERHUB_CREDENTIALS_PSW | docker login -u $DOCKERHUB_CREDENTIALS_USR --password-stdin'
 docker push assignment/springapp:$BUILD_NUMBER
 docker logout
