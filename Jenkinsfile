ec37dd3d8cb744414215579833022d41c88d15af
root <root@ip-172-31-95-140.ec2.internal>
2022-11-04 13:23:12 +0000

    adding function to send email

--- Jenkinsfile
+++ Jenkinsfile
@@ -34,5 +34,12 @@ pipeline {
             }
          }
  }
-}
 
+post {
+        always {
+            emailext body: 'A Test EMail', recipientProviders: [[$class: 'DevelopersRecipientProvider'], [$class: 'RequesterRecipientProvider']], subject: 'Test'
+        }
+   
+
+}
+}
--- artifacts.sh
+++ artifacts.sh
@@ -9,3 +9,13 @@ echo " Login Dockerhub"
 echo $DOCKERHUB_CREDENTIALS_PSW | docker login -u $DOCKERHUB_CREDENTIALS_USR --password-stdin
 docker push gurpreet0706/springapp:$BUILD_NUMBER
 docker logout
+
+
+
+
+#emailext (
+#     subject: "SUCCESSFUL: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]'",
+#     body: """<p>SUCCESSFUL: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]':</p>
+#       <p>Check console output at &QUOT;<a href='${env.BUILD_URL}'>${env.JOB_NAME} [${env.BUILD_NUMBER}]</a>&QUOT;</p>""",
+#     recipientProviders: [[$class: 'DevelopersRecipientProvider']]
+#   )
