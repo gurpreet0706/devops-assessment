cd3f82c3a98f1e70f5f0033669ff56b852d21d7c
root <root@ip-172-31-95-140.ec2.internal>
2022-11-04 09:59:31 +0000

    added node

--- build.sh
+++ build.sh
@@ -5,6 +5,7 @@ mvn clean install
 cd target
 pushd .
 nohup java -jar my-app-0.0.1-SNAPSHOT.jar > /dev/null 2>&1 &
+sleep 240
 serverPID=$!
 newman run ../../DevOps-Assessment.postman_collection.json
 kill serverPID
