291e5d704cac882adbef8d6792d95ea9195375d4
root <root@ip-172-31-95-140.ec2.internal>
2022-11-04 10:26:31 +0000

    added all the stages

---
+++ artifacts.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+echo artifacts building
--- build.sh
+++ build.sh
@@ -2,12 +2,12 @@
 
 cd my-app
 mvn clean install
-cd target
-pushd .
-nohup java -jar my-app-0.0.1-SNAPSHOT.jar > /dev/null 2>&1 &
-serverPID=$!
-sleep 2m
-newman run ../../DevOps-Assessment.postman_collection.json
-kill $serverPID
+#cd target
+#pushd .
+#nohup java -jar my-app-0.0.1-SNAPSHOT.jar > /dev/null 2>&1 &
+#serverPID=$!
+#sleep 2m
+#newman run ../../DevOps-Assessment.postman_collection.json
+#kill $serverPID
 
 
